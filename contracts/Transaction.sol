eff5b907ced5da71152bb24bcc0d656a6dad82f3
ElserManuel <elser.lazaro@vallegrande.edu.pe>
2025-03-12 09:15:02 -0500

    test-camp

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
-// @title un simulador de una Transacción
-// @author Juan Condori :D
-// @notice puede usar este contrato solo para la simulación de una Transacción
 
 contract Transaction {
     
-    function sendRemittance(address payable _remittanceRecipient, uint256 _remittanceAmount) public payable {        
-        _remittanceRecipient.transfer(_remittanceAmount);
+    function sendRemittance(address payable _recipient, uint256 _amount) public payable {        
+        _recipient.transfer(_amount);
     }
 
 }
