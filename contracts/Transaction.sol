62e6cf4cd67ddffe3088da12a26d224e2558b989
ElserManuel <elser.lazaro@vallegrande.edu.pe>
2025-03-12 09:17:58 -0500

    test

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -3,8 +3,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Transaction {
     
-    function sendRemittance(address payable _recipient, uint256 _amount) public payable {        
-        _recipient.transfer(_amount);
-    }
+    event RemittanceSent(address indexed sender, address indexed recipient, uint256 amount);
+    
+    function sendRemittance(address payable _recipient, uint256 _amount) public payable {
+        require(_amount > 0, "El monto debe ser mayor a 0");
+        require(address(this).balance >= _amount, "Fondos insuficientes en el contrato");
 
+        _recipient.transfer(_amount);
+
+        emit RemittanceSent(msg.sender, _recipient, _amount);
+    }
 }
