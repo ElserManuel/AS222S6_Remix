4f7a9ca5e6f6285b28abddbe7eab2cd9e4dd5747
ElserManuel <elser.lazaro@vallegrande.edu.pe>
2025-03-26 20:40:27 -0500

    version smasll

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Transaction {
-    
+import "solmate/src/utils/SafeTransferLib.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Transaction is Ownable {
+    using SafeTransferLib for address payable;
+
     event RemittanceSent(address indexed sender, address indexed recipient, uint256 amount);
-    
-    function sendRemittance(address payable _recipient, uint256 _amount) public payable {
+
+    constructor() Ownable(msg.sender) {}
+
+    function sendRemittance(address payable _recipient, uint256 _amount) public payable onlyOwner {
         require(_amount > 0, "El monto debe ser mayor a 0");
         require(address(this).balance >= _amount, "Fondos insuficientes en el contrato");
 
-        _recipient.transfer(_amount);
+        _recipient.safeTransferETH(_amount);
 
         emit RemittanceSent(msg.sender, _recipient, _amount);
     }
